7ca6f913854f070cca59f348c1909c9e5bc50a55
julianbopp <julian.bopp@tutanota.com>
2022-07-28 10:41:15 +0200

    implement game loop

--- external.js
+++ external.js
@@ -5,7 +5,7 @@ function getComputerChoice() {
     return choices[r]
 }
 
-function rps(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase()
 
     if (playerSelection === computerSelection) {
@@ -37,6 +37,25 @@ function rps(playerSelection, computerSelection) {
     }
 }
 
-console.log(rps("Rock",getComputerChoice()))
-console.log(rps("Paper",getComputerChoice()))
-console.log(rps("Scissors",getComputerChoice()))
+function game() {
+      let playerScore = 0
+      let computerScore = 0
+
+      for (let i = 0; i < 5; i++) {
+        let playerInput = prompt("Put in your choice (Rock, Paper, Scissors)")
+        let result = playRound(playerInput, getComputerChoice())
+        console.log(result.slice(4,5))
+
+        if (result.slice(4,5) === "W") {
+            playerScore = playerScore + 1
+        } else if (result.slice(4,5) === "L")  {
+            computerScore = computerScore + 1
+        }
+
+        console.log(playerScore)
+        console.log(computerScore)
+
+
+      }
+}
+game()
