cd9d555a9dd94f3fb12390004d640ffab141e332
julianbopp <julian.bopp@tutanota.com>
2022-07-29 15:53:56 +0200

    remove old game logic

--- external.js
+++ external.js
@@ -30,33 +30,4 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == 3 && computerSelection == 2) {
         return 1;
     }
-    playerSelection = playerSelection.charAt(0).toUpperCase()
-
-    if (playerSelection === computerSelection) {
-        return "Tie!";
-    }
 }
-
-
-function game() {
-      let playerScore = 0
-      let computerScore = 0
-
-      for (let i = 0; i < 5; i++) {
-        let playerInput = prompt("Put in your choice (Rock, Paper, Scissors)")
-        let result = playRound(playerInput, getComputerChoice())
-        console.log(result.slice(4,5))
-
-        if (result.slice(4,5) === "W") {
-            playerScore = playerScore + 1
-        } else if (result.slice(4,5) === "L")  {
-            computerScore = computerScore + 1
-        }
-
-        console.log(playerScore)
-        console.log(computerScore)
-
-
-      }
-}
-game()
